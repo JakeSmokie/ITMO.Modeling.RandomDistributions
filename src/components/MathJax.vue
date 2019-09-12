49839aa9fbcdceea117bbee834f4d9b98af233eb
JakeSmokie <jakesmokie@gmail.com>
2019-09-12 23:54:33 +0300

    Add MathJax component.

---
+++ src/components/MathJax.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <vue-mathjax :formula="formula"></vue-mathjax>
+    <div class="slot-wrapper d-none">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MathJax",
+
+    data() {
+      return {
+        formula: ''
+      }
+    },
+
+    updated() {
+      this.formula = `$$ ${this.$el.getElementsByClassName('slot-wrapper')[0].innerHTML} $$`;
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
