48f1f0a1813be6dde38e088846ea0c3068aef369
JakeSmokie <jakesmokie@gmail.com>
2019-09-12 23:51:09 +0300

    Remove junk code.

--- src/main.js
+++ src/main.js
@@ -6,13 +6,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-require('mathjax').init({
-  loader: {load: ['input/tex', 'output/svg']}
-}).then((MathJax) => {
-  const svg = MathJax.tex2svg('\\frac{1}{x^2-1}', {display: true});
-  console.log(MathJax.startup.adaptor.outerHTML(svg));
-}).catch((err) => console.log(err.message));
-
 import VueMathjax from 'vue-mathjax'
 Vue.use(VueMathjax);
 
