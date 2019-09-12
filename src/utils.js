e481c0e0efc371641615a31856a3a72d6270d312
JakeSmokie <jakesmokie@gmail.com>
2019-09-13 00:40:56 +0300

    Add debounce util.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,18 @@
 export function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function debounce(f, interval) {
+  let timer = null;
+
+  return (...args) => {
+    clearTimeout(timer);
+
+    return new Promise(resolve =>
+      timer = setTimeout(
+        () => resolve(f(...args)),
+        interval,
+      )
+    );
+  };
+}
