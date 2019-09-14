2e0b687f0bb0e975cc8c21ae40bd50d9d70af42e
JakeSmokie <jakesmokie@gmail.com>
2019-09-14 23:05:44 +0300

    Copy uniform to gamma.

--- src/views/GammaDistribution.vue
+++ src/views/GammaDistribution.vue
@@ -1,92 +1,108 @@
 <template>
   <b-container
     fluid
-    class="mt-4"
+    class="p-4"
   >
-    <b-card class="main mx-auto">
-      <b-input-group prepend="Ваше ФИО">
-        <b-form-input
-          :state="nameState"
-          aria-describedby="input-live-help input-live-feedback"
-          autofocus
-          id="input-live"
-          trim
-          v-model="name"
-          v-on:update="generateValues()"
-        ></b-form-input>
-        <b-form-invalid-feedback id="input-live-feedback">
-          Введённая строка не является ФИО
-        </b-form-invalid-feedback>
-      </b-input-group>
-
-      <b-card
-        no-body
-        class="p-2 mt-4"
-      >
-        <b-form-group
-          class="mb-0"
-          label-cols="3"
-          description="Количество генерируемых значений"
-          label-for="range"
-          label-align="center"
-        >
-          <template v-slot:label>
-            <katex class="mt-2">N = {{ valuesCount }}</katex>
-          </template>
-
+    <div class="d-flex flex-row align-items-start">
+      <b-card class="main text-center">
+        <b-input-group prepend="Ваше ФИО">
           <b-form-input
-            class="pt-3 px-3"
-            id="range"
-            v-model="valuesCount"
-            type="range"
-            min="50"
-            max="10000"
-            step="50"
-            v-on:change="generateValues()"
-          />
-        </b-form-group>
-      </b-card>
+            :state="nameState"
+            aria-describedby="input-live-help input-live-feedback"
+            autofocus
+            id="input-live"
+            trim
+            v-model="name"
+            v-on:update="generateValues()"
+          ></b-form-input>
+          <b-form-invalid-feedback id="input-live-feedback">
+            Введённая строка не является ФИО
+          </b-form-invalid-feedback>
+        </b-input-group>
 
-      <template v-if="coefficients">
-        <div class="mt-4 d-flex flex-row">
-          <div class="mr-4 text-nowrap text-left">
-            <b-card>
-              <katex>
-                Ф = {{ fullName.surname.length || 0 }} \\
-                И = {{ fullName.name.length }} \\
-                О = {{ fullName.fatherName.length }} \\ \: \\
+        <b-card
+          no-body
+          class="p-2 mt-4"
+        >
+          <b-form-group
+            class="mb-0"
+            label-cols="3"
+            description="Количество генерируемых значений"
+            label-for="range"
+            label-align="center"
+          >
+            <template v-slot:label>
+              <katex class="mt-2">N = {{ valuesCount }}</katex>
+            </template>
 
-                {{ fullCoefficientsFormula }}
+            <b-form-input
+              class="pt-3 px-3"
+              id="range"
+              v-model="valuesCount"
+              type="range"
+              min="50"
+              max="10000"
+              step="50"
+              v-on:change="generateValues()"
+            />
+          </b-form-group>
+        </b-card>
+
+        <template v-if="coefficients">
+          <div class="mt-4 d-flex flex-row">
+            <div class="mr-4 text-nowrap text-left">
+              <b-card>
+                <katex>
+                  Ф = {{ fullName.surname.length || 0 }} \\
+                  И = {{ fullName.name.length }} \\
+                  О = {{ fullName.fatherName.length }} \\ \: \\
+
+                  {{ fullCoefficientsFormula }}
+                </katex>
+              </b-card>
+            </div>
+            <b-card class="w-100">
+              <katex class="my-auto text-center">
+                V = {{ coefficientsValues.VariationCoefficient | truncate }} \\
+                M_{теор} = {{ coefficientsValues.Expected }} \\ \: \\
+                \sigma_{теор} = {{ standardDerivation | truncate }} \\
+                D_{теор} = {{ variance | truncate }} \\ \: \\
+                l = {{ radius | truncate }} \\
+                a = {{ leftEdge | truncate }} \\
+                b = {{ rightEdge | truncate }} \\
               </katex>
             </b-card>
           </div>
-          <b-card class="w-100">
-            <katex class="my-auto">
 
+          <b-card class="mt-4">
+            <katex>
+              V = \frac{\sigma}{\overline{x}} \qquad
+              \sigma = V * \overline{x} = V * M =
+              {{ coefficientsValues.VariationCoefficient | truncate }} * {{ coefficientsValues.Expected }} =
+              {{ standardDerivation | truncate }} \\ \: \\
+
+              D = \sigma^2 = {{ variance | truncate }} \\ \: \\
+              D_{uniform} = \frac{1}{12}(b - a)^2 = \frac{1}{12}(M + l - (M - l))^2 = \frac{1}{3}l^2 \\ \: \\
+              \frac{1}{3}l^2 = {{ variance | truncate }} \qquad
+              l = \sqrt{ {{ variance | truncate }} * 3 } = \sqrt{ {{ variance * 3 | truncate }} } = {{ radius | truncate }}
             </katex>
           </b-card>
-        </div>
 
-        <b-card class="mt-4">
-          <katex>
-
-          </katex>
-        </b-card>
-
-        <b-button
-          v-if="false"
-          block
-          variant="outline-primary"
-          class="mt-4"
-          v-on:click="generateValues()"
-        >
-          Сгенерировать значения
-        </b-button>
-      </template>
-    </b-card>
-
-    <div v-if="values.length > 0">
-      <b-card class="main mx-auto mt-4 text-left">
+          <b-button
+            v-if="false"
+            block
+            variant="outline-primary"
+            class="mt-4"
+            v-on:click="generateValues()"
+          >
+            Сгенерировать значения
+          </b-button>
+        </template>
+      </b-card>
+      <b-card
+        v-if="values.length > 0"
+        class="main ml-4 text-left"
+      >
         <katex>
           M_{экс} = \sum{x_i * p_i} = {{ actualExpectedValue | truncate }} \\
           D_{экс} = M[x^2] - M^2[x] = {{ actualVariance | truncate }} \\
@@ -101,6 +117,9 @@
 
         </katex>
       </b-card>
+    </div>
+
+    <div v-if="values.length > 0">
       <b-card class="mt-4">
         <line-chart
           :chartdata="densityChart"
@@ -119,6 +138,7 @@
           :options="options"
         />
       </b-card>
+      <div class="mt-4"></div>
     </div>
   </b-container>
 </template>
@@ -246,15 +266,18 @@
           labels: this.densityHistogram(step).map(([k]) => k),
           datasets: [{
             label: 'Actual density',
-            backgroundColor: 'rgba(248,121,121, 0.3)',
-            data: this.densityHistogram(step).map(([, xs]) => xs)
+            backgroundColor: 'rgba(0,220,24,0.3)',
+            data: this.densityHistogram(step)
+              .map(([, xs]) => xs)
+              .map(x => x.toFixed(3))
           }, {
             label: 'Expected density',
-            backgroundColor: 'rgba(0,255,180,0.3)',
+            backgroundColor: 'rgba(92,95,90,0.3)',
             data: this.densityHistogram(step)
               .map(([k]) => k)
               .map(this.calcSectionLength(step))
               .map(k => k / (2 * this.radius))
+              .map(x => x.toFixed(3))
           }]
         }
       },
@@ -266,17 +289,17 @@
           labels: this.densityHistogram(step).map(([k]) => k),
           datasets: [{
             label: 'Actual distribution',
-            backgroundColor: 'rgba(248,121,121, 0.3)',
+            backgroundColor: 'rgba(0,220,24,0.3)',
             data: this.calcDistribution(this.densityHistogram(step).map(([, density]) => density))
-              .map(x => x.toFixed(3))
+              .map(x => x.toFixed(4))
           }, {
             label: 'Expected distribution',
-            backgroundColor: 'rgba(0,255,180,0.3)',
+            backgroundColor: 'rgba(92,95,90,0.3)',
             data: this.calcDistribution(this.densityHistogram(step)
               .map(([k]) => k)
               .map(this.calcSectionLength(step))
               .map(k => k / (2 * this.radius))
-            ).map(x => x.toFixed(3))
+            ).map(x => x.toFixed(4))
           }]
         }
       },
@@ -289,11 +312,11 @@
           labels: histogram.map(([k]) => k),
           datasets: [{
             label: 'Actual count',
-            backgroundColor: 'rgba(248,121,121, 0.3)',
+            backgroundColor: 'rgba(0,220,24,0.3)',
             data: histogram.map(([, xs]) => xs)
           }, {
             label: 'Expected count',
-            backgroundColor: 'rgba(0,255,180,0.3)',
+            backgroundColor: 'rgba(92,95,90,0.3)',
             data: histogram
               .map(([k]) => k)
               .map(this.calcSectionLength(step))
@@ -348,7 +371,7 @@
     methods: {
       async generateValues() {
         this.values = [];
-        await sleep(50);
+        await sleep(0);
 
         const mt = MersenneTwister19937.seed(this.coefficientsValues.Seed);
         const random = () => real(this.leftEdge, this.rightEdge, true)(mt);
